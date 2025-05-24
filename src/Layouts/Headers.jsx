0a4bc360b9e0868bf8262a84bcae9da0a3a5c24f
Kaushik <kaushik573.balar@gmail.com>
2025-05-25 03:43:14 +0530

    implemented new UI

---
+++ src/Layouts/Headers.jsx
@@ -0,0 +1,103 @@
+import "./layouts.css";
+
+export const Headers = () => {
+  return (
+    <>
+      <header class="header">
+        <div class="header-top">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="header-top-wrapper">
+                  <div class="header-top-info">
+                    <div class="email">
+                      <div class="icon">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16.95"
+                          height="13.4"
+                          viewBox="0 0 16.95 13.4"
+                        >
+                          <g id="Mail" transform="translate(0.975 0.7)">
+                            <path
+                              id="Path_1"
+                              data-name="Path 1"
+                              d="M3.5,4h12A1.5,1.5,0,0,1,17,5.5v9A1.5,1.5,0,0,1,15.5,16H3.5A1.5,1.5,0,0,1,2,14.5v-9A1.5,1.5,0,0,1,3.5,4Z"
+                              transform="translate(-2 -4)"
+                              fill="none"
+                              stroke="#1a2224"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="1.4"
+                            />
+                            <path
+                              id="Path_2"
+                              data-name="Path 2"
+                              d="M17,6,9.5,11.25,2,6"
+                              transform="translate(-2 -4.5)"
+                              fill="none"
+                              stroke="#1a2224"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="1.4"
+                            />
+                          </g>
+                        </svg>
+                      </div>
+                      <div class="text">
+                        <span>support@mail.com</span>
+                      </div>
+                    </div>
+                    <div class="cta">
+                      <div class="icon">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="13.401"
+                          height="13.401"
+                          viewBox="0 0 13.401 13.401"
+                        >
+                          <g
+                            id="Phone_Icon"
+                            data-name="Phone Icon"
+                            transform="translate(0.7 0.7)"
+                          >
+                            <path
+                              id="Phone_Icon-2"
+                              data-name="Phone Icon"
+                              d="M14.111,10.984v1.806A1.206,1.206,0,0,1,12.8,14a11.956,11.956,0,0,1-5.207-1.849,11.754,11.754,0,0,1-3.62-3.613A11.9,11.9,0,0,1,2.117,3.313,1.205,1.205,0,0,1,3.317,2h1.81A1.206,1.206,0,0,1,6.334,3.036a7.719,7.719,0,0,0,.422,1.692A1.2,1.2,0,0,1,6.485,6l-.766.765a9.644,9.644,0,0,0,3.62,3.613l.766-.765a1.208,1.208,0,0,1,1.273-.271,7.76,7.76,0,0,0,1.7.422,1.205,1.205,0,0,1,1.038,1.222Z"
+                              transform="translate(-2.112 -2)"
+                              fill="none"
+                              stroke="#1a2224"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="1.4"
+                            />
+                          </g>
+                        </svg>
+                      </div>
+                      <div class="text">
+                        <span>+8801658 874521</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="header-top-info">
+                    <div class="email">
+                      <div class="text">
+                        <span>Help & FAQs</span>
+                      </div>
+                    </div>
+                    <div class="cta">
+                      <div class="text">
+                        <span>My Account</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
